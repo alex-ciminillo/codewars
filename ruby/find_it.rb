5e6f874cbf3144954543252b8123d728d11e19fc
ciminilloa <ciminilloa@findlay.edu>
2021-12-01 04:30:27 -0500

    Add one-liner for #find_it

--- ruby/find_it.rb
+++ ruby/find_it.rb
@@ -1,9 +1,14 @@
 
-#first attempt
+# #first attempt
+# def find_it(seq)
+#   hash_counter = Hash.new(0)
+#   seq.each { |num| hash_counter[num] += 1 }
+#   hash_counter.each { |k,v| return k if v.odd? }
+# end
+
+#one-liner
 def find_it(seq)
-  hash_counter = Hash.new(0)
-  seq.each { |num| hash_counter[num] += 1 }
-  hash_counter.each { |k,v| return k if v.odd? }
+    seq.detect { |num| seq.count(num).odd? }    
 end
 
 p find_it([1,2,2,3,3,3,4,3,3,3,2,2,1])
