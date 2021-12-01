ec9b11e24d5bef1b0983b330086090bbdb0c634e
ciminilloa <ciminilloa@findlay.edu>
2021-11-30 20:50:23 -0500

    Optimize #disemvowel

--- python/disemvowel.py
+++ python/disemvowel.py
@@ -8,3 +8,9 @@ def disemvowel(string_):
             new_string += i
     print(new_string)
     return new_string
+
+#optimized
+def disemvowel(string_):
+    "".join(c for c in str if c.lower() not in "aeiou")
+
+str = 'I am a super cool dude! Look at me!'
