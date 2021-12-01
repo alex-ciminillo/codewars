c2840d1e2b979b3bd341e9a3f3cbf9424528ec56
ciminilloa <ciminilloa@findlay.edu>
2021-12-01 04:20:59 -0500

    Optimize #high_and_low

--- python/high_and_low.py
+++ python/high_and_low.py
@@ -1,17 +1,23 @@
 
-#first_attempt
+# #first_attempt
+# def high_and_low(numbers):
+#     numbers = numbers.split(' ')
+#     high = numbers[0]
+#     low = numbers[0]
+#     numbers = numbers[1:]
+#     print(numbers)
+#     for i in numbers:
+#         if int(i) > int(high):
+#             high = i
+#         if int(i) < int(low):
+#             low = i
+#     return f'{high} {low}'
+
+#optimized
 def high_and_low(numbers):
-    numbers = numbers.split(' ')
-    high = numbers[0]
-    low = numbers[0]
-    numbers = numbers[1:]
-    print(numbers)
-    for i in numbers:
-        if int(i) > int(high):
-            high = i
-        if int(i) < int(low):
-            low = i
-    return f'{high} {low}'
+    numbers = [int(num) for num in numbers.split(" ")]
+    return "%i %i" % (max(numbers), min(numbers)) 
+    
 
 
 print(high_and_low("8 3 -5 42 -1 0 0 -9 4 7 4 -4"))  # return "5 1"
