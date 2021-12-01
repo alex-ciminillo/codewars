a797551f5421d2e1eb83c535a2e81fb6dc3ff3a0
ciminilloa <ciminilloa@findlay.edu>
2021-12-01 05:56:43 -0500

    Optimize #accum

--- ruby/accum.rb
+++ ruby/accum.rb
@@ -8,16 +8,21 @@
 # The parameter of accum is a string which 
 #includes only letters from a..z and A..Z.
 
-#first-attempt
+# #first-attempt
+# def accum(s)
+# 	accum_str = ""
+#     s.each_char.with_index do |char, idx|
+#         (idx + 1).times do |num|
+#             num.zero? ? accum_str += char.upcase : accum_str += char.downcase 
+#         end
+#         accum_str += "-" unless idx == (s.length - 1)
+#     end
+#     accum_str
+# end
+
+#optimized
 def accum(s)
-	accum_str = ""
-    s.each_char.with_index do |char, idx|
-        (idx + 1).times do |num|
-            num.zero? ? accum_str += char.upcase : accum_str += char.downcase 
-        end
-        accum_str += "-" unless idx == (s.length - 1)
-    end
-    accum_str
+	s.split("").map.with_index { |char, idx| char.upcase + char.downcase*idx }.join("-")
 end
 
 p accum("abcd")
