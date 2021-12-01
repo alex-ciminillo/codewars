e40c3ce74cc915ef8b0cf4957be5c0ffaa30e3da
ciminilloa <ciminilloa@findlay.edu>
2021-12-01 05:24:23 -0500

    Optimize #order

--- python/order.py
+++ python/order.py
@@ -1,17 +1,20 @@
 
-#first attempt
-def order(sentence):
-    if sentence == "":
-        return ""
-    order_dict = {}
-    for word in sentence.split(" "):
-        for char in word:
-            if char.isdigit():
-                order_dict[int(char)] = word
-    ordered_str = []
-    for i in range(1, (len(sentence.split(" ")) + 1)):
-        ordered_str += [order_dict[i]]
-    return ' '.join(ordered_str)
+# #first attempt
+# def order(sentence):
+#     if sentence == "":
+#         return ""
+#     order_dict = {}
+#     for word in sentence.split(" "):
+#         for char in word:
+#             if char.isdigit():
+#                 order_dict[int(char)] = word
+#     ordered_str = []
+#     for i in range(1, (len(sentence.split(" ")) + 1)):
+#         ordered_str += [order_dict[i]]
+#     return ' '.join(ordered_str)
 
+def order(sentence):
+    return ' '.join(sorted(sentence.split(), key=lambda w:sorted(w)))
+    
 str = "is2 Thi1s T4est 3a"
 print(order(str))
