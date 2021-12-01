ae9a02f2033eff501c49f5f8b72a4c57262ee234
ciminilloa <ciminilloa@findlay.edu>
2021-12-01 03:55:59 -0500

    Add one-liner to #duplicate_encode

--- python/duplicate_encode.py
+++ python/duplicate_encode.py
@@ -1,20 +1,24 @@
 from typing import DefaultDict
 
-
-def duplicate_encode(word):
-    #your code here
-    a = {}
-    a = DefaultDict(lambda:0,a)
-    para_str = ""
-    for i in word:
-        a[i.lower()] += 1
-    for i in word:
-        if a[i.lower()] > 1:
-            para_str += ")"
-        else:
-            para_str += "("
-    return para_str
+# #first attempt 
+# def duplicate_encode(word):
+#     a = {}
+#     a = DefaultDict(lambda:0,a)
+#     para_str = ""
+#     for i in word:
+#         a[i.lower()] += 1
+#     for i in word:
+#         if a[i.lower()] > 1:
+#             para_str += ")"
+#         else:
+#             para_str += "("
+#     return para_str
     
+
+#one_liner
+def duplicate_encode(word):
+    return "".join("(" if word.lower().count(c) == 1 else ")" for c in word.lower())
+
 
     
 
