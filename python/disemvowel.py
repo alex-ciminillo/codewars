1b8e1fcd65b1854fe4ab69c8638aea840774e262
ciminilloa <ciminilloa@findlay.edu>
2021-11-30 21:03:23 -0500

    Add new approach to #disemvowel

--- python/disemvowel.py
+++ python/disemvowel.py
@@ -1,16 +1,21 @@
 
-#first attempt
-def disemvowel(string_):
-    vowels = 'aeiou'
-    new_string = ""
-    for i in string_:
-        if i.lower() not in vowels:
-            new_string += i
-    print(new_string)
-    return new_string
+# #first attempt
+# def disemvowel(string_):
+#     vowels = 'aeiou'
+#     new_string = ""
+#     for i in string_:
+#         if i.lower() not in vowels:
+#             new_string += i
+#     print(new_string)
+#     return new_string
 
-#optimized
-def disemvowel(string_):
-    "".join(c for c in str if c.lower() not in "aeiou")
+# #optimized
+# def disemvowel(string_):
+#     return "".join(c for c in str if c.lower() not in "aeiou")
+
+#optimized 2
+def disemvowel(s):
+    return s.translate(str.maketrans('','',"aeiouAEIOU"))
 
 str = 'I am a super cool dude! Look at me!'
+print(disemvowel(str))
