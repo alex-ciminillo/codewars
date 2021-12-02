53881f2620c71bd7f7b7397a5f6578bec68adaaf
ciminilloa <ciminilloa@findlay.edu>
2021-12-02 10:21:01 -0500

    Optimize #toCamelCase

--- javascript/8_kyu/to_camel_case.js
+++ javascript/8_kyu/to_camel_case.js
@@ -1,21 +1,28 @@
+
+// // first attempt 
+// function toCamelCase(str){
+//     i = 0
+//     new_str = ""
+//     dash = false
+//     while (i < str.length) {
+//         if (str[i] == "-" || str[i] == "_") {
+//             new_str += ""
+//             dash = true
+//         } else if (dash) {
+//             new_str += str[i].toUpperCase()
+//             dash = false
+//         } else {
+//             new_str += str[i]
+//         }
+//         i += 1
+//     }
+//     return new_str
+// }
+
 function toCamelCase(str){
-    i = 0
-    new_str = ""
-    dash = false
-    while (i < str.length) {
-        if (str[i] == "-" || str[i] == "_") {
-            new_str += ""
-            dash = true
-        } else if (dash) {
-            new_str += str[i].toUpperCase()
-            dash = false
-        } else {
-            new_str += str[i]
-        }
-        i += 1
-    }
-    return new_str
-}
+    var regExp = /[-_](.)/g
+    return str.replace(regExp, (_, c) => c.toUpperCase());
+  }
 
 
-console.log(toCamelCase("the-stealth-warrior"))
+console.log(toCamelCase("the-stealth-warrior-me"))
